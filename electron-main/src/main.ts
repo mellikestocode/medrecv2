5f8fca7fa421c097e547af3f598e0aa21bec46a9
mellikestocode <mel.canela@icloud.com>
2025-03-31 20:34:08 +0530

    Initial stages of MedRec v2 with updated libraries and modern tooling

---
+++ electron-main/src/main.ts
@@ -0,0 +1,40 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: true,
+    },
+  });
+
+  // In development, load from the dev server
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    // In production, load the built file
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
