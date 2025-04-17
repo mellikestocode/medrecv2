28094dfbc1ff28d8539fa440f608e97d84087e08
Melvin Canela <mel.canela@icloud.com>
2025-04-17 13:35:33 -0400

    added empty tests

---
+++ renderer/src/Dashboard.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+describe('Dashboard', () => {
+    describe('Patient Panel', () => {
+        
+    })
+    describe('Medication Panel', () => {
+
+    })
+    describe('Allergy Panel', () => {
+
+    })
+})
