c277b09b6d11bda95d2e5805200785db7c7e40ef
Melvin Canela <mel.canela@icloud.com>
2025-04-25 12:39:41 -0400

    Implement fetchPatient function to return mock patient data

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,5 @@
+import { Gender, Patient } from "../renderer/src/types";
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const fetchToken = async (username: string, password: string): Promise<string> => {
@@ -8,11 +10,47 @@ export const fetchToken = async (username: string, password: string): Promise<st
         },
         body: JSON.stringify({ username, password }),
     });
-    
+
     if (!response.ok) {
         throw new Error(response.statusText);
     }
-    
+
     const data = await response.json();
     return data.token;
+}
+
+export const fetchPatient = async (): Promise<Patient> => {
+    // const token = await localStorage.getItem('token');
+    // if (!token) {
+    //     throw new Error('No token found');
+    // }
+    // const response = await fetch(`${apiUrl}/patient`, {
+    //     method: 'GET',
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         'Authorization': `Bearer ${token}`,
+    //     }
+    // }
+    // );
+    // if (!response.ok) {
+    //     throw new Error(response.statusText);
+    // }
+    // const data = await response.json();
+    // return data;
+    return {
+        name: 'John Doe',
+        dob: '01/01/1990',
+        gender: Gender.Male,
+        height: 180,
+        weight: 75,
+        medicalHistory: [
+            {
+                dateOfService: '01/01/2023',
+                createdAt: '01/02/2023',
+                provider: 'Dr. Smith',
+                notes: 'Follow-up appointment',
+                medications: ['Medication 1', 'Medication 2'],
+            },
+        ],
+    }
 }
