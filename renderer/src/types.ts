7a2adf2e540a9a167417477ae3e68f9f0f98df7c
Melvin Canela <mel.canela@icloud.com>
2025-04-25 12:21:56 -0400

    Add Gender enum and MedicalHistory interface to types

--- renderer/src/types.ts
+++ renderer/src/types.ts
@@ -1,4 +1,22 @@
+export enum Gender {
+    Male = 'Male',
+    Female = 'Female',
+    Other = 'Other',
+}
+
+export interface MedicalHistory {
+    dateOfService: string;
+    createdAt: string;
+    provider: string;
+    notes: string;
+    medications: string[];
+}
+
 export interface Patient {
     name: string;
     dob: string;
+    gender: Gender;
+    height: number;
+    weight: number;
+    medicalHistory: MedicalHistory[];
 }
